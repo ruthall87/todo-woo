ba1162f75a7879206ad69f46c4dfa0f78581403f
Eleanor Hall <ruthhall97@gmail.com>
2020-12-31 13:22:49 -0800

    ADDED DELETE AND COMPLETE BUTTON

--- todo/views.py
+++ todo/views.py
@@ -59,7 +59,7 @@ def createtodo(request):
             form = TodoForm(request.POST)
             newtodo = form.save(commit=False)
             newtodo.user = request.user
-            
+            newtodo.save()
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data passed in. Try again.'})
@@ -67,43 +67,43 @@ def createtodo(request):
 
 @login_required
 def currenttodos(request):
-    todo = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, 'todo/currenttodos.html', {'todo': todo})
+    
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    print(todos)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
 
 
 @login_required
 def completedtodos(request):
-    todo = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, 'todo/completedtodos.html', {'todo': todo})
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
 
 
 @login_required
 def viewtodo(request, todo_pk):
-    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    todos = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'GET':
-        form = TodoForm(instance=todo)
-        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+        form = TodoForm(instance=todos)
+        return render(request, 'todo/viewtodo.html', {'todos': todos, 'form': form})
     else:
         try:
-            form = TodoForm(request.POST, instance=todo)
+            form = TodoForm(request.POST, instance=todos)
             form.save()
             return redirect('currenttodos')
         except ValueError:
-            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
+            return render(request, 'todo/viewtodo.html', {'todos': todos, 'form': form, 'error': 'Bad info'})
 
 
 @login_required
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
-    if request.method == 'POST':
-        todo.datecompleted = timezone.now()
-        todo.save()
-        return redirect('currenttodos')
+    todo.datecompleted = timezone.now()
+    todo.save()
+    return redirect('currenttodos')
 
 
 @login_required
 def deletetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
-    if request.method == 'POST':
-        todo.delete()
-        return redirect('currenttodos')
+    todo.delete()
+    return redirect('currenttodos')
